33f7db7474d3830cd2acc3603c22bcb6d0419937
azzahraayu832 <azzahraayuv0832@gmail.com>
2022-03-23 20:27:33 +0700

    perbaikifungsinilai

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function getInputValue() {
   var nama = document.getElementById("nama").value;
   var nim = document.getElementById("nim").value;
   var nm_mk = document.getElementById("nm_mk").value;
+  var outputnilai = document.getElementById("outputnilai");
   var ket;
 
   a = (tugas * 15) / 100;
@@ -14,51 +15,57 @@ function getInputValue() {
   d = (uas * 35) / 100;
   var totalnilai = 0;
   totalnilai = a + b + c + d;
-
+console.log(totalnilai)
 //grading scale
   if(totalnilai <100 && totalnilai>=92 ){
     ip = "A";
     ket = 'Istimewa';
-    document.getElementById("output").style.background = "#A6DBBC";
+    ////document.getElementById("output").style.background = "#A6DBBC";
 } else if (totalnilai<91 && totalnilai>= 86){
     ip = "A-";
     ket = 'Hampir Istimewa';
-    document.getElementById("output").style.background = "#AFD69B"
+    //document.getElementById("output").style.background = "#AFD69B"
 } else if (totalnilai<85 && totalnilai>= 81){
     ip = "B+";
     ket = 'Baik Sekali';
-    document.getElementById("output").style.background = "#A3C3E7"
+    //document.getElementById("output").style.background = "#A3C3E7"
 } else if (totalnilai<80 && totalnilai>= 76){
     ip = "B";
     ket = 'Baik';
-    document.getElementById("output").style.background = "B9D7EA"
+    //document.getElementById("output").style.background = "B9D7EA"
 } else if (totalnilai<75 && totalnilai>= 71){
     ip = "B-";
     ket = 'Cukup Baik';
-    document.getElementById("output").style.background = "#FDEB37"
+    //document.getElementById("output").style.background = "#FDEB37"
 } else if (totalnilai<70 && totalnilai>= 66){
     ip = "C+";
     ket= 'Lebih dari Cukup';
-    document.getElementById("output").style.background = "#FFF487"
+    //document.getElementById("output").style.background = "#FFF487"
 } else if (totalnilai<65 && totalnilai>= 60){
     ip = "C";
     ket = 'Cukup';
-    document.getElementById("output").style.background = "#FFF487"
+    //document.getElementById("output").style.background = "#FFF487"
 } else if (totalnilai<59 && totalnilai>= 55){
     ip = "D";
     ket = 'Kurang ';
-    document.getElementById("output").style.background = "#B22B27"
+    //document.getElementById("output").style.background = "#B22B27"
 } else {
     ip = "E";
     ket = 'GAGAL';
-    document.getElementById("output").style.background = "#C71E1E"
+    //document.getElementById("output").style.background = "#C71E1E"
 }
 
-document.getElementById("hasil").innerHTML = ip;
-document.getElementById("totalnilai").innerHTML = totalnilai;
+$("#hasil").html(ip)
+$("#outputnilai").html(totalnilai)
+$("#nama2").html(nama)
+$("#nim2").html(nim)
+$("#keterangan").html(ket)
+
+/*document.getElementById("hasil").innerHTML = ip;
+document.getElementById("output").innerHTML = totalnilai;
 document.getElementById("nama2").innerHTML = nama;
 document.getElementById("nim2").innerHTML = nim;
-document.getElementById("keterangan").innerHTML = ket;
+document.getElementById("keterangan").innerHTML = ket;*/
 
 }
 
